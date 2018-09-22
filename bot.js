8650e7498ad737ebf5ad4dd9ff0e48d59e5c4fb1
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-22 12:32:27 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,10 +23,9 @@ client.on("message", async message => {
   if (cont === 'a') {
       if (message.member.roles.has(357371636539981824) || message.member.roles.has(357371681985134592) || message.member.roles.has(474073433202884618) || message.member.roles.has(357610506946609153) || message.member.roles.has(459102161310318632) || allbypass === true) {
           const warning = await message.reply("you're sending " + cont + " a bit too quickly! Messages weren't deleted because you can bypass.");
-          setTimeout(function() {warning.delete()}, dwa)
       } else {
           message.reply("you're sending emote only messages too quickly!")
-      }
+      } setTimeout(function() {warning.delete()}, dwa);
   }
     
     

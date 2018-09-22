7a0bfab2135cedf30e1986e21261ddc1e8fc4230
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-22 09:17:35 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,19 +21,13 @@ client.on("message", async message => {
     if (tagname === 'all') {
       message.channel.send({embed: {
         color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
         description: "\n",
         fields: [{
             name: "All Tags",
             value: "globalsnotworking, noglobalshere, expiredinvite, mentions, mixeremotes"
           },
         ],
-        timestamp: new Date(),
         footer: {
-        icon_url: message.author.avatarURL,
         text: `Requested by ${message.author.tag}`
         }
       }
@@ -42,11 +36,7 @@ client.on("message", async message => {
     
   if (tagname === 'globalsnotworking') {
    message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
+        color: 16562432,
         description: "\n",
         fields: [{
             name: "Why aren't the Global Emotes working even though I joined the server?",
@@ -55,7 +45,6 @@ client.on("message", async message => {
         ],
         timestamp: new Date(),
         footer: {
-        icon_url: message.author.avatarURL,
         text: `Tag name: globalsnotworking`
         }
       }
@@ -64,20 +53,14 @@ client.on("message", async message => {
     
  if (tagname === 'noglobalshere') {
    message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
+        color: 16562432,
         description: "\n",
         fields: [{
             name: "Why aren't the global emotes on here working?",
             value: "This server does not have global emotes; we list servers that have them. If you want to donate, so we can get global emotes, send <@193979517470113792> a direct message."
           },
         ],
-        timestamp: new Date(),
-        footer: {
-        icon_url: message.author.avatarURL,
+        footer: {,
         text: `Tag name: noglobalshere`
         }
       }
@@ -86,20 +69,14 @@ client.on("message", async message => {
     
  if (tagname === 'pings' || tagname === 'mentions') {
    message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
+        color: 16562432,
         description: "\n",
         fields: [{
             name: "How do I stop a server from mentioning me?",
             value: "If the owner of the server is doing (@)everyone or (@)here, you can right-click the server icon and click Notification Settings then choose mute server and suppress (@)everyone and (@)here. If the owner is tagging a role, check if the role is self-assignable and remove the role using the command for the said bot."
         },
         ],
-        timestamp: new Date(),
         footer: {
-        icon_url: message.author.avatarURL,
         text: `Tag name: pings`
         }
       }
@@ -108,20 +85,14 @@ client.on("message", async message => {
     
   if (tagname === 'expiredinvite') {
    message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
+        color: 16562432,
         description: "\n",
         fields: [{
             name: "Why is the global emote server's invite expired for me?",
             value: "One of the main reasons is that the invite is just expired, but another reason is that you're on 100 servers, and you need to leave a few, so the invites aren't expired. There's a chance that you were banned from the server because someone listed you on the user-made discord ban database used by bots like ServerHound."
         },
         ],
-        timestamp: new Date(),
         footer: {
-        icon_url: message.author.avatarURL,
         text: `Tag name: expiredinvite`
         }
       }
@@ -130,20 +101,14 @@ client.on("message", async message => {
     
  if (tagname === 'mixeremotes') {
    message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
+        color: 16562432,
         description: "\n",
         fields: [{
             name: "What are Mixer global emotes?",
             value: "Mixer, just like GameWisp and BTTV are discord integrations that give servers the ability to have global emotes. Unfortunately, the Mixer integration is terrible and leads to small-sized emotes becoming enlarged on Discord causing them to look like pixelated trash."
         },
         ],
-        timestamp: new Date(),
         footer: {
-        icon_url: message.author.avatarURL,
         text: `Tag name: mixeremotes`
         }
       }

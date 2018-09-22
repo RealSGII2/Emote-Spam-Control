0875484d5d348dbc926d599df5711fab03c389f9
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-22 09:13:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,10 +25,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "All Tags",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "All Tags",
             value: "globalsnotworking, noglobalshere, expiredinvite, mentions, mixeremotes"
           },
         ],
@@ -48,10 +47,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "Why aren't the Global Emotes working even though I joined the server?",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "Why aren't the Global Emotes working even though I joined the server?",
             value: "Emotes failing to work depends on the integration: GameWisp requires you to have a role on the server--some global emote servers will automatically give you this role. If the role isn't automatically given, you'll have to add it yourself manually. If you've met the above requirement, you may need to restart your discord. Mixer emotes servers, and BTTV emote servers require none of this hassle."
           },
         ],
@@ -71,10 +69,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "Why aren't the global emotes on here working?",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "Why aren't the global emotes on here working?",
             value: "This server does not have global emotes; we list servers that have them. If you want to donate, so we can get global emotes, send <@193979517470113792> a direct message."
           },
         ],
@@ -94,10 +91,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "How do I stop a server from pinging me?",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "How do I stop a server from mentioning me?",
             value: "If the owner of the server is doing (@)everyone or (@)here, you can right-click the server icon and click Notification Settings then choose mute server and suppress (@)everyone and (@)here. If the owner is tagging a role, check if the role is self-assignable and remove the role using the command for the said bot."
         },
         ],
@@ -117,10 +113,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "Why is the global emote server's invite expired for me?",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "Why is the global emote server's invite expired for me?",
             value: "One of the main reasons is that the invite is just expired, but another reason is that you're on 100 servers, and you need to leave a few, so the invites aren't expired. There's a chance that you were banned from the server because someone listed you on the user-made discord ban database used by bots like ServerHound."
         },
         ],
@@ -140,10 +135,9 @@ client.on("message", async message => {
           name: client.user.username,
           icon_url: client.user.avatarURL
         },
-        title: "Why is the global emote server's invite expired for me?",
         description: "\n",
         fields: [{
-            name: "\n",
+            name: "What are Mixer global emotes?",
             value: "Mixer, just like GameWisp and BTTV are discord integrations that give servers the ability to have global emotes. Unfortunately, the Mixer integration is terrible and leads to small-sized emotes becoming enlarged on Discord causing them to look like pixelated trash."
         },
         ],

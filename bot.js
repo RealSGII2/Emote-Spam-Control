f90a77ff8ed9b151674e263854d2082637f4d834
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-23 07:19:09 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ client.on("message", async message => {
           setTimeout(function() {
           client.channels.get("360179378333941761").send(`${message.author.tag} has emote spammed. I delete their messages.`);
           const warning = message.reply("you're sending emote only messages too quickly!");
-          setTimeout(function() {warning.delete()}, dwa);}, 3000);
+          setTimeout(function() {warning.delete()}, dwa);}, 1500);
       } 
       }
   }

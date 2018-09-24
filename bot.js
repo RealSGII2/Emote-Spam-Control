8e118074debf4e68db8cea8a593be415b9164819
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-09-24 15:25:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,6 +47,10 @@ client.on("message", async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase()
   
+  if (command === 'allowbypass' || command === 'setwarningdel') {
+  
+  if (allowedids.includes(message.author.id)) {
+      
   if (command === 'allowbypass') {
       const setting = args.shift();
       if (setting === 'true' || setting === 'false') {allbypass = setting; message.reply(`the option to all bypass for everyone set to **${allbypass.toString()}**!`)} else {message.reply("the settings must either be: `['true', 'false']`!")}
@@ -62,7 +66,7 @@ client.on("message", async message => {
       }
   }
       
-  } else {message.reply("you can't use that command!")}
+  } else {message.reply("you can't use that command!")}}
   
   if (command === 'ping') {
    const m = await message.channel.send("Pinging...");

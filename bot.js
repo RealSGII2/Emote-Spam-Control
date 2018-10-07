74d84461288e98a4d28900514163357452b5b543
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-07 14:49:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -189,8 +189,8 @@ client.on("message", async message => {
 	  let role = args[0];
 	  if (!role) return message.reply("please say a role to make mentionable or unmentionable.");
 	  if (message.guild.roles.find("name", role)) {
-		  if (!message.guild.roles.find("name", role).mentionable) {message.guild.roles.find("name", role).setMentionable(false, `Requested by ${message.author.tag}.`); message.reply(`${role} is now mentionable.!`);}
-		  if (message.guild.roles.find("name", role).mentionable) {message.guild.roles.find("name", role).setMentionable(true, `Requested by ${message.author.tag}.`); message.reply(`${role} is no longer mentionable.`);}
+		  if (message.guild.roles.find("name", role).mentionable) {message.guild.roles.find("name", role).setMentionable(false, `Requested by ${message.author.tag}.`).then(() => { message.reply(`${role} is no longer mentionable.`);}).catch(err => message.channel.send(`Err! ${err}`)});}
+		  if (!message.guild.roles.find("name", role).mentionable) {message.guild.roles.find("name", role).setMentionable(true, `Requested by ${message.author.tag}.`).then(() => { message.reply(`${role} is now mentionable!`);}).catch(err => message.channel.send(`Err! \`${err}\``)});}
 		  
 		  } else {
 	message.reply("can't find that role! Make sure you say the full name.");

c18edcdc9698102f2df44fc6afe384b184f8cc1a
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-10-14 00:12:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -319,10 +319,11 @@ client.on("message", async message => {
   }
 	
   if (command === 'help') {
+	         let pg = args[0];
 		 let rep = "Help command. You need to give me a page number, else I can't give you any commands! Say `!help <page 1 - 2>` for a page, like this: `!help 1`.";
 		 if (args[0] === "1") {rep = "`+sub` Submit a bug or suggestion. \n`+help` Shows this menu. \n`+ping` Shows the bot's ping. \n`+stats` Views stats. \n`+tag <name | all>` Uses Global Emote Hunter's tag system and sends them. \n`+mentionable <role>` Changes a role's mentionable state. \n`+mpadd <user>` Adds a user to the list who can use the mentionable command. \n`+rtest` ReGex test."}
 		 if (args[0] === "2") {rep = "`+invite` Information on invites."}
-		 if (args[0] === "") {rep = ""}
+		 if (args[0] === "undefined") {pg = "information (0)"}
 	  message.channel.send({embed: {
     				color: 3066993,
     				description: "\n",
@@ -331,7 +332,7 @@ client.on("message", async message => {
    				    value: rep
 				},
   				],
-		  footer: {text: "Viewing page " + args[0]}
+		  footer: {text: "Viewing page " + pg + "."}
 				}
 			});
   }
